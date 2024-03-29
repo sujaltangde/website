fbc025e7018ec245e95beeb7c5cb18839cbb2393
harshalhonde21 <harshalhonde17@gmail.com>
2024-03-29 17:59:14 +0530

    updated the about us

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,25 +1,18 @@
-import React from 'react'
-import { Hero } from '../components/Home/Hero'
-import { Projects } from '../components/Home/Projects'
-import About from "../components/Home/About"
+import React from "react";
+import { Hero } from "../components/Home/Hero";
+import { Projects } from "../components/Home/Projects";
+import About from "../components/Home/About";
 
 export const Home = () => {
   return (
     <>
-    
-        <div className='min-h-screen bg-gray-900 pt-20'>
+      <div className="min-h-screen bg-gray-900 pt-20">
+        <Hero />
 
+        <About />
 
-        <Hero/>
-
-        <About/>
-
-        <Projects/>
-
-         
-
-        </div>
-
+        <Projects />
+      </div>
     </>
   );
 };

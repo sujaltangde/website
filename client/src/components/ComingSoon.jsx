71f2b4ddc913d8c21cae12013e86ed7f87eb98ba
Sujal Tangde <101286994+sujaltangde@users.noreply.github.com>
2024-03-30 09:40:57 +0530

    Update ComingSoon.jsx

--- client/src/components/ComingSoon.jsx
+++ client/src/components/ComingSoon.jsx
@@ -4,7 +4,7 @@ export const ComingSoon = () => {
   return (
     <Fragment>
       <main className="center-container flex gap-3  md:gap-9 md:pb-0 pb-28 items-center text-4xl md:text-9xl">
-        <span style={{fontFamily:'Mona Sans', fontWeight:'600'}}>Coming</span><span style={{fontFamily:'Mona Sans', fontWeight:'600'}} className="gradient-text py-4 ">Soon</span>
+        <span style={{fontFamily:'Mona Sans', fontWeight:'700'}}>Coming</span><span style={{fontFamily:'Mona Sans', fontWeight:'700'}} className="gradient-text py-4 ">Soon</span>
       </main>
     </Fragment>
   );

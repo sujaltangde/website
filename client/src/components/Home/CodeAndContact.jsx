5005e746500fc1a9e9322f1266b1272e0b167bcf
harshalhonde21 <harshalhonde17@gmail.com>
2024-03-31 15:09:00 +0530

    intital changes

--- client/src/components/Home/CodeAndContact.jsx
+++ client/src/components/Home/CodeAndContact.jsx
@@ -1,41 +1,53 @@
-import React from 'react';
+import React from "react";
 import "../../styles/CodeAndContact.css";
 
 const CodeAndContact = React.memo(() => {
-    return (
-        <main className='codeOfConduct-container'>
-            <section>
-                <h1 className="heading-code">Code of Conduct</h1>
-                <h2 className="heading-para-code">Fostering an Open and Welcoming Community</h2>
-                <p className='paragraph-codeConduct'>CNCF's Code of Conduct and Linux Foundation's Events Code of Conduct applies to KCD Chennai and its Organizers, Volunteers, Chapter members, Speakers, Staff and Attendees. Report violations to the organizers by contacting organizers-chennai@kubernetescomrnunitydays.org or the CNCF staff at conduct@cncf.io.</p>
-            </section>
+  return (
+    <main className="codeOfConduct-container">
+      <section>
+        <h1 className="heading-code">Code of Conduct</h1>
+        <h2 className="heading-para-code">
+          Fostering an Open and Welcoming Community
+        </h2>
+        <p className="paragraph-codeConduct">
+          CNCF's Code of Conduct and Linux Foundation's Events Code of Conduct
+          applies to KCD Chennai and its Organizers, Volunteers, Chapter
+          members, Speakers, Staff and Attendees. Report violations to the
+          organizers by contacting
+          organizers-chennai@kubernetescomrnunitydays.org or the CNCF staff at
+          conduct@cncf.io.
+        </p>
+      </section>
 
-            <section>
-                <h1 className="heading-code">Contact Us</h1>
-                <h2 className="heading-para-code">Connect with our community</h2>
-                <div className="contact-component-code">
-                    <div className="section-contact">
-                        <img src="discord.png" alt="image loading.." />
-                        <h4 className="head-contact">Discord</h4>
-                        <h5 className="para-contacts">discord.com</h5>
-                    </div>
-                    <div className="section-contact">
-                        <img src="mail.png" alt="image loading.." />
-                        <h4 className="head-contact">Email</h4>
-                        <h5 className="para-contacts">nagpur@cncf.io</h5>
-                    </div>
-                    <div className="section-contact">
-                        <img src="whatapps.png" alt="image loading.." />
-                        <h4 className="head-contact">WhatsApp</h4>
-                        <h5 className="para-contacts">whatsapp.invite@cncf_nagpur</h5>
-                    </div>
-                </div>
-            </section>
-        </main>
+      <section>
+        <h1 className="heading-code">Contact Us</h1>
+        <h2 className="heading-para-code">Connect with our community</h2>
+        <div className="contact-component-code">
+          <div className="section-contact">
+            <img src="discord.png" alt="image loading.." />
+            <h4 className="head-contact">Discord</h4>
+            <h5 className="para-contacts">discord.com</h5>
+          </div>
 
-    )
-})
+          <div className="line-contact"></div>
 
-CodeAndContact.displayName = 'CodeAndContact';
+          <div className="section-contact">
+            <img src="mail.png" alt="image loading.." />
+            <h4 className="head-contact">Email</h4>
+            <h5 className="para-contacts">nagpur@cncf.io</h5>
+          </div>
+          <div className="line-contact"></div>
+          <div className="section-contact">
+            <img src="whatapps.png" alt="image loading.." />
+            <h4 className="head-contact">WhatsApp</h4>
+            <h5 className="para-contacts">whatsapp.invite@cncf_nagpur</h5>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+});
 
-export default CodeAndContact
+CodeAndContact.displayName = "CodeAndContact";
+
+export default CodeAndContact;

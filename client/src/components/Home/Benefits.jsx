9445224581e4634a8d9e0b237cb8087d39f6073e
harshalhonde21 <harshalhonde17@gmail.com>
2024-04-02 17:19:45 +0530

    udpated css

--- client/src/components/Home/Benefits.jsx
+++ client/src/components/Home/Benefits.jsx
@@ -1,30 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "../../styles/Benefits.css";
 
 const Benefits = React.memo(() => {
-
     const [hoveredIndex, setHoveredIndex] = useState(null);
 
     const handleHover = (index) => {
         setHoveredIndex(index);
     };
 
-
     const slides = [
         {
             key: 1,
-            content: <div className='text-white'>1</div>
+            content: <div className="text-white">1</div>,
         },
         {
             key: 2,
-            content: <div className='text-white'>2</div>
+            content: <div className="text-white">2</div>,
         },
         {
             key: 3,
-            content: <div className='text-white'>3</div>
-        }
+            content: <div className="text-white">3</div>,
+        },
     ];
-
 
     return (
         <main>
@@ -33,30 +30,28 @@ const Benefits = React.memo(() => {
             </div>
 
             <div className="flex justify-center items-center h-screen">
-                {[0, 1, 2].map((index) => (
+                {slides.map((slide) => (
                     <div
-                        key={index}
-                        className={`transition-all duration-300 relative ${hoveredIndex !== null && hoveredIndex !== index
-                                ? 'opacity-50 transform scale-90'
-                                : ''
-                            } ${hoveredIndex === index
-                                ? 'transform scale-110 shadow-lg bg-blue-200'
-                                : 'bg-blue-400'
+                        key={slide.key}
+                        className={`transition-all duration-300 relative ${hoveredIndex !== null && hoveredIndex !== slide.key
+                                ? "opacity-50 transform scale-90"
+                                : ""
+                            } ${hoveredIndex === slide.key
+                                ? "transform scale-110 shadow-lg bg-blue-200"
+                                : "bg-blue-400"
                             } w-32 h-32 m-2 flex items-center justify-center`}
-                        onMouseEnter={() => handleHover(index)}
+                        onMouseEnter={() => handleHover(slide.key)}
                         onMouseLeave={() => setHoveredIndex(null)}
                     >
                         <div
                             className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"
                             style={{ zIndex: 1 }}
                         >
-                            <p>{index + 1}</p>
+                            {slide.content}
                         </div>
                     </div>
                 ))}
             </div>
-
-
         </main>
     );
 });
@@ -64,8 +59,3 @@ const Benefits = React.memo(() => {
 Benefits.displayName = "Benefits";
 
 export default Benefits;
-
-
-
-
-
